b04423ce1e9add60512dcd661ac1fc3d941d3804
Stuart Creed <stuart.a.creed@gmail.com>
2021-09-11 18:19:05 +0100

    Vuex example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,11 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+
+    <button @click="increaseHunger">
+        Click me to increase hunger
+    </button>
+
   </div>
 </template>
 
@@ -35,6 +40,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    increaseHunger() {
+      return this.$store.dispatch('eatSandwich')
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
+import Vuex from 'vuex';
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+      hungerLevel: 0
+    },
+    mutations: {
+      INCREASE_HUNGER_LEVEL(state) {
+        state.hungerLevel++
+      }
+    },
+    actions: {
+      eatSandwich(context) {
+        return context.commit('INCREASE_HUNGER_LEVEL')
+      }
+    }
+})
+
+
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
+
+
